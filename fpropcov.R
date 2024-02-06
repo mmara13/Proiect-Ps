b5d2b31eeb67bb5acd0449f5d709687968a1335c
Oana-Maria Cochiorca <116117096+oanamaria10@users.noreply.github.com>
2024-02-06 17:40:55 +0200

    Update fpropcov.R

--- fpropcov.R
+++ fpropcov.R
@@ -1,12 +1,18 @@
 
 fpropcov <- function(a, b, c, d, repartitie_comuna, rep_marginal_x, rep_marginal_y) {
-  # Calculați varianțele pentru X și Y
-  var_x <- sum(rep_marginal_x * seq_along(rep_marginal_x))
-  var_y <- sum(rep_marginal_y * seq_along(rep_marginal_y))
-  
-  # Calculați E[X], E[Y] și E[X*Y]
+  # Calculați E[X], E[Y], E[X^2], E[Y^2]
   medie_x <- sum(rep_marginal_x * seq_along(rep_marginal_x))
   medie_y <- sum(rep_marginal_y * seq_along(rep_marginal_y))
+  medie_xx <- sum(rep_marginal_x * seq_along(rep_marginal_x)^2)
+  medie_yy <- sum(rep_marginal_y * seq_along(rep_marginal_y)^2)
+  
+  # Calculați varianțele pentru X și Y
+  # Var(X) = E[X^2] - E[X]^2
+  var_x <- medie_xx - medie_x^2
+  # Var(Y) = E[Y^2] - E[Y]^2
+  var_y <- medie_yy - medie_y^2
+  
+  # Calculați E[X*Y]
   medie_xy <- sum(repartitie_comuna * (seq_along(rep_marginal_x) %*% t(seq_along(rep_marginal_y))))
   
   # Calculează covarianța utilizând formula cov(X,Y) = E[X*Y] - E[X]*E[Y]
@@ -14,7 +20,7 @@ fpropcov <- function(a, b, c, d, repartitie_comuna, rep_marginal_x, rep_marginal
   
   # Covarianța pentru Z și T
   # cov(Z,T) = cov(a*X+b*Y,c*X+d*Y) = a*c*Var(X) + b*c*cov(X,Y) + b*d*Var(Y)
-  cov_zt <- a * c * var_x + b * c * cov_xy + b * d * var_y
+  cov_zt <- round(a * c * var_x + b * c * cov_xy + b * d * var_y,3)
   
   return(cov_zt)
 }
@@ -48,10 +54,10 @@ rep_marginal_y[sample(n, n * 0.3)]
 print("Repartitie comuna (matrice):")
 print(repartitie_comuna)
 
-print("Repartitie marginala pe x (sume normalizate):")
+print("Repartitie marginala pe x:")
 print(rep_marginal_x)
 
-print("Repartitie marginala pe y (sume normalizate):")
+print("Repartitie marginala pe y:")
 print(rep_marginal_y)
 
 # Exemplu de utilizare pentru fpropcov
